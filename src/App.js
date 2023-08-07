d6a54e2db7f0a56c119e8c0b48f830d3894cfb18
Vinoth30457 <vinoth30457@gmail.com>
2023-08-07 07:54:18 +0530

    add js

--- src/App.js
+++ src/App.js
@@ -351,37 +351,7 @@ const App = () => {
               and correct.
             </p>
           </div>
-          {/* <div className="inputfield" required>
-            <div>
-              <script src="https://www.google.com/recaptcha/api.js"></script>
-              <div
-                className="g-recaptcha"
-                data-sitekey="6LdAvUIUAAAAAHjrjmjtNTcXyKm0WKwefLp-dQv9"
-              ></div>
-              <noscript>
-                <div>
-                  <div style="width: 302px; height: 422px; position: relative;">
-                    <div style="width: 302px; height: 422px; position: absolute;">
-                      <iframe
-                        src="https://www.google.com/recaptcha/api/fallback?k=6LdAvUIUAAAAAHjrjmjtNTcXyKm0WKwefLp-dQv9"
-                        frameborder="0"
-                        scrolling="no"
-                        style="width: 302px; height:422px; border-style: none;"
-                      ></iframe>
-                    </div>
-                  </div>
-                  <div style="width: 300px; height: 60px; border-style: none; bottom: 12px; left: 25px; margin: 0px; padding: 0px; right: 25px; background: #f9f9f9; border: 1px solid #c1c1c1; border-radius: 3px;">
-                    <textarea
-                      id="g-recaptcha-response"
-                      name="g-recaptcha-response"
-                      className="g-recaptcha-response"
-                      style="width: 250px; height: 40px; border: 1px solid #c1c1c1; margin: 10px 25px; padding: 0px; resize: none;"
-                    ></textarea>
-                  </div>
-                </div>
-              </noscript>
-            </div>
-          </div> */}
+
           <ReCAPTCHA
             sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
             onChange={handleChange}
